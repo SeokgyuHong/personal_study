ba21f234f34429c4cc813954a88d6d6baed3e707
seokgyuHong <hsg8974@naver.com>
2021-01-03 01:09:59 +0900

    프로그래머스 문제풀이 추가

---
+++ 프로그래머스/lev2_스킬트리.py
@@ -0,0 +1,14 @@
+def solution(skill, skill_trees):
+    answer = 0
+    dic = {}
+    for temp in skill:
+        dic[temp] = 0
+    if 'c' in dic:
+        print('c')
+    # for temp in skill_trees:
+    #     for i in temp:
+    #         for item in dic.items():
+    #             if
+    #             dic[temp]=1
+
+    return answer
